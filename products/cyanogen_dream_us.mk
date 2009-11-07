a9410106d4835007450fa4b5eb7f34b37311cdb8
Steve Kondik <shade@chemlab.org>
2009-11-07 12:14:58 -0500

    Add compcache enable/disable script.

--- products/cyanogen_dream_us.mk
+++ products/cyanogen_dream_us.mk
@@ -34,5 +34,6 @@ PRODUCT_COPY_FILES += vendor/cyanogen/etc/apns-conf.xml:system/etc/apns-conf.xml
                       vendor/cyanogen/etc/init.d/99complete:system/etc/init.d/99complete \
                       vendor/cyanogen/bin/fix_permissions:system/bin/fix_permissions \
                       vendor/cyanogen/bin/usb-tether:system/bin/usb-tether \
-		      vendor/cyanogen/bin/shutdown:system/bin/shutdown
+					  vendor/cyanogen/bin/shutdown:system/bin/shutdown \
+					  vendor/cyanogen/bin/compcache:system/bin/compcache
 
