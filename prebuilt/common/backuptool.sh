1a7addff7e310905b6f69a98b2526f5a46a046c6
Steve Kondik <shade@chemlab.org>
2010-02-22 11:57:28 -0500

    Cleanup/beautify shell scripts.

--- prebuilt/common/backuptool.sh
+++ prebuilt/common/backuptool.sh
@@ -51,65 +51,65 @@ EOF
 }
 
 backup_file() {
-    if [ -e "$1" ];
-    then
-        if [ -n "$2" ];
-        then
-            echo "$2  $1" | md5sum -c -
-            if [ $? -ne 0 ];
-            then
-                echo "MD5Sum check for $1 failed!";
-                exit $?;
-            fi
-        fi
-
-        local F=`basename $1`
-
-        # dont backup any apps that have odex files, they are useless
-        if ( echo $F | grep -q "\.apk$" ) && [ -e `echo $1 | sed -e 's/\.apk$/\.odex/'` ];
-        then
-            echo "Skipping odexed apk $1";
-        else    
-            cp $1 $C/$F
-        fi
-    fi
+   if [ -e "$1" ];
+   then
+      if [ -n "$2" ];
+      then
+         echo "$2  $1" | md5sum -c -
+         if [ $? -ne 0 ];
+         then
+            echo "MD5Sum check for $1 failed!";
+            exit $?;
+         fi
+      fi
+      
+      local F=`basename $1`
+      
+      # dont backup any apps that have odex files, they are useless
+      if ( echo $F | grep -q "\.apk$" ) && [ -e `echo $1 | sed -e 's/\.apk$/\.odex/'` ];
+      then
+         echo "Skipping odexed apk $1";
+      else
+         cp $1 $C/$F
+      fi
+   fi
 }
 
 restore_file() {
-    local FILE=`basename $1`
-    local DIR=`dirname $1`
-    if [ -e "$C/$FILE" ];
-    then
-        if [ ! -d "$DIR" ];
-        then
-            mkdir -p $DIR;
-        fi
-        cp -p $C/$FILE $1;
-        if [ -n "$2" ];
-        then
-            rm $2;
-        fi
-    fi
+   local FILE=`basename $1`
+   local DIR=`dirname $1`
+   if [ -e "$C/$FILE" ];
+   then
+      if [ ! -d "$DIR" ];
+      then
+         mkdir -p $DIR;
+      fi
+      cp -p $C/$FILE $1;
+      if [ -n "$2" ];
+      then
+         rm $2;
+      fi
+   fi
 }
 
 case "$1" in
-    backup)
-        mount $S
-        mount $C
-        get_files | while read FILE REPLACEMENT; do
-            backup_file $S/$FILE
-        done
-        ;;
-    restore)
-        get_files | while read FILE REPLACEMENT; do
-            R=""
-            [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
-            restore_file $S/$FILE $R
-        done
-        ;;
-    *) 
-        echo "Usage: $0 {backup|restore}"
-        exit 1
+   backup)
+      mount $S
+      mount $C
+      get_files | while read FILE REPLACEMENT; do
+         backup_file $S/$FILE
+      done
+   ;;
+   restore)
+      get_files | while read FILE REPLACEMENT; do
+         R=""
+         [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+         restore_file $S/$FILE $R
+      done
+   ;;
+   *)
+      echo "Usage: $0 {backup|restore}"
+      exit 1
 esac
 
 exit 0
