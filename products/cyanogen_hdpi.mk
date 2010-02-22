26542ed7341df6fb2ac521b7fdd989df23627956
Steve Kondik <shade@chemlab.org>
2010-02-22 12:21:04 -0500

    Change userinit path.

--- products/cyanogen_hdpi.mk
+++ products/cyanogen_hdpi.mk
@@ -97,7 +97,7 @@ PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/cyanogen/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
     vendor/cyanogen/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-    vendor/cyanogen/prebuilt/common/etc/init.d/05userinit:system/etc/init.d/05userinit \
+    vendor/cyanogen/prebuilt/common/etc/init.d/20userinit:system/etc/init.d/20userinit \
     vendor/cyanogen/prebuilt/common/etc/init.d/99complete:system/etc/init.d/99complete \
     vendor/cyanogen/prebuilt/common/bin/fix_permissions:system/bin/fix_permissions \
     vendor/cyanogen/prebuilt/common/bin/usb-tether:system/bin/usb-tether \
