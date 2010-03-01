04d2c0f3d293be1eacdadf3121cc9b837caab9fe
Koushik K. Dutta <koushd@gmail.com>
2010-02-28 22:25:48 -0800

    INCLUDE_BOTH_LAUNCHERS did not actually work, as that is picked up by every product. PRODUCT_COPY_FILES works, though it is a bit hacky. oh well

--- products/cyanogen_sholes.mk
+++ products/cyanogen_sholes.mk
@@ -24,9 +24,9 @@ PRODUCT_MODEL := Droid
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=ERE25 BUILD_DISPLAY_ID="voles-userdebug 2.1 ERE25 23960 test-keys" PRODUCT_NAME=voles TARGET_DEVICE=sholes BUILD_FINGERPRINT=verizon/voles/sholes/sholes:2.0.1/ESD56/20996:user/release-keys PRODUCT_BRAND=verizon
 
-INCLUDE_BOTH_LAUNCHERS := true
-
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/sholes-open/media/bootanimation.zip:system/media/bootanimation.zip
+    
+PRODUCT_COPY_FILES += out/target/product/sholes-open/obj/APPS/Launcher_intermediates/package.apk:/system/app/Launcher.apk
 
 include vendor/motorola/sholes-open/device_sholes.mk
