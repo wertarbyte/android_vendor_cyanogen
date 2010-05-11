fbe2b6e568e89675ce67b982745c8215711133ab
Steve Kondik <shade@chemlab.org>
2010-05-11 19:57:39 -0400

    Update D/S to test4

--- products/cyanogen_dream_sapphire.mk
+++ products/cyanogen_dream_sapphire.mk
@@ -35,7 +35,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-5-$(shell date +%m%d%Y)-NIGHTLY-DS
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-5.0.7-DS-test3
+        ro.modversion=CyanogenMod-5.0.7-DS-test4
 endif
 
 include frameworks/base/data/sounds/AudioPackage4.mk 
