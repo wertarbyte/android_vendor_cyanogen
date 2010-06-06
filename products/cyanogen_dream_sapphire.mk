503268e5594a312a7b7445f44646f9b371390dce
Steve Kondik <shade@chemlab.org>
2010-06-06 01:23:38 -0400

    Bump to 5.0.8-test2

--- products/cyanogen_dream_sapphire.mk
+++ products/cyanogen_dream_sapphire.mk
@@ -35,7 +35,7 @@ ifdef CYANOGEN_NIGHTLY
         ro.modversion=CyanogenMod-5-$(shell date +%m%d%Y)-NIGHTLY-DS
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-5.0.8-DS-test1
+        ro.modversion=CyanogenMod-5.0.8-DS-test2
 endif
 
 include frameworks/base/data/sounds/AudioPackage4.mk 
